26d5785e6eb1d057cc5c96c5d29c88c8eb75f50d
vikinjorge <jorgema@gmail.com>
2017-02-24 20:12:58 +0100

    Terminado en lo básico, falta ponerlo bonito

--- js/motor.js
+++ js/motor.js
@@ -1,24 +1,46 @@
-////comprueba que la cuadricula no sea demasiado pequeña ni demasiado grande
-var compruebaCuadricula = function(){
-	while (numCuadros<4 || numCuadros>64)
-	{
-		 numCuadros = prompt("Introduce el tamaño de cuadricula min 4 y max 64");
-	}
-};
-
-
-//calcula el ancho que tienen que tener los cuadros para caber en container principal
-var calculaCuadricula = function()
+$(document).ready(function()
 {
-	//division tamaño cuadricula entre cuadros indicados
-	ancho = cuadricula/numCuadros;
 
-	///multiplicamos los cuadros para tener el total de cudros que habrá que dibujar
-	///si puso 4, pues 4x4=16, 16 cuadros dibujaremos
-	numCuadros=numCuadros*numCuadros;
-};
+	////comprueba que la cuadricula no sea demasiado pequeña ni demasiado grande
+	var compruebaCuadricula = function()
+	{
+		while (numCuadros<4 || numCuadros>64)
+		{
+			 numCuadros = prompt("Introduce el tamaño de cuadricula min 4 y max 64");
+		}
+	};
 
 
+	//calcula el ancho que tienen que tener los cuadros para caber en container principal
+	var calculaCuadricula = function()
+	{
+		//division tamaño cuadricula entre cuadros indicados
+		ancho = cuadricula/numCuadros;
+
+		///multiplicamos los cuadros para tener el total de cudros que habrá que dibujar
+		///si puso 4, pues 4x4=16, 16 cuadros dibujaremos
+		numCuadros=numCuadros*numCuadros;
+
+		for (var fil=0; fil<numCuadros; fil++)
+		{
+
+				$('#container').append('<div id="celdaF'+ fil+'" class="celda"></div>');
+				$('.celda').width(ancho);
+				$('.celda').height(ancho);
+		}
+	};
+
+	var colorAlAzar = function (){
+
+			r= Math.floor(Math.random()*255)+1;
+			g= Math.floor(Math.random()*255)+1;
+			b= Math.floor(Math.random()*255)+1;
+
+
+	};
+	
+
+$('#esMultiColor').prop("checked",true);
 
 var numCuadros = prompt("Introduce el tamaño de cuadricula min 4 y max 64");
 compruebaCuadricula();
@@ -31,30 +53,12 @@ var multiColor = true;
 var r = 0;
 var g = 0;
 var b = 0;
+calculaCuadricula();
 
 
-
-
-
-
-$(document).ready(function(){
-	
-	calculaCuadricula();
-
-
-
-
-	for (var fil=0; fil<numCuadros; fil++)
-	{
-
-			$('#container').append('<div id="celdaF'+ fil+'" class="celda"></div>');
-			$('.celda').width(ancho);
-			$('.celda').height(ancho);
-
-	}
-
-
-$('.celda').mouseenter(function(){
+$('body').on('mouseenter', '.celda',function()
+{
+	console.log("has pasado");
 
 	if (multiColor)
 	{
@@ -66,30 +70,43 @@ $('.celda').mouseenter(function(){
 		$(this).css("background-color", "rgb(0,0,0");
 		console.log(multiColor);
 	}
-	
-
-
 });
 
-/*$('.celda').mouseout(function(){
-
-	$(this).css("background-color", "rgb("+r-10+","+g-10+","+b-10+")");
-
-})*/
-
-
-var colorAlAzar = function (){
-
-		r= Math.floor(Math.random()*255)+1;
-		g= Math.floor(Math.random()*255)+1;
-		b= Math.floor(Math.random()*255)+1;
-
-
-};
 
 
 
 
+
+$('#botonReset').click(function()
+{
+		$('#container').empty();
+		
+
+			if ($('#esMultiColor').is(":checked"))
+			{
+				multiColor=true;
+				console.log("ha entrado en True");
+			}
+			else
+			{
+				multiColor=false;
+				console.log("ha entrado en false");
+			}
+
+		numCuadros=0;
+		cuadricula = 400;
+		ancho = 0;
+		compruebaCuadricula();
+		calculaCuadricula();
+		console.log("multiColor vale"+multiColor);
+});
+
+
+
+
+
+
+	
 
 
 });
